b865b4d4d33ef6b4a397bef440eb8b5fa1924403
Jake Waletzko <jake.waletzko@sportngin.com>
2016-08-26 11:35:25 -0500

    testing

--- app/models/github_to_slack/slack.rb
+++ app/models/github_to_slack/slack.rb
@@ -2,6 +2,8 @@ module GithubToSlack
   class Slack < ActiveRecord::Base
    
     def notify(message)
+      puts message
+      puts "#{webhook_url}, #{channel_name}, #{user_name}"
       Slack::Notifier
         .new(webhook_url,
           channel: channel_name,
