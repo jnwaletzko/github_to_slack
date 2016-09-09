770daa6ceee33212c22adf03718706c0317778ca
Jake Waletzko <jake.waletzko@sportngin.com>
2016-09-09 15:03:50 -0500

    encrypt slack url

--- app/models/github_to_slack/slacker.rb
+++ app/models/github_to_slack/slacker.rb
@@ -2,6 +2,8 @@ module GithubToSlack
   require 'slack-notifier'
   class Slacker < ActiveRecord::Base
     belongs_to :repository
+
+    attr_encrypted :ssn, key: "sU2*We8Z8hpd7GwGdkDu!DqLmkAOf0Pk8pO7f^&R"
     
     def notify(message)
       Slack::Notifier
--- github_to_slack.gemspec
+++ github_to_slack.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency "rails", "~> 4.2"
   s.add_dependency "slack-notifier"
+  s.add_dependency "attr_encrypted", "~> 3.0.0"
 
   s.add_development_dependency "sqlite3"
 end
