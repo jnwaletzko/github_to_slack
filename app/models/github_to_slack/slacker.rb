57aa61f0804f9d5b5c1c1ae1dac13f798e51d43d
Jake Waletzko <jake.waletzko@sportngin.com>
2016-09-09 15:45:59 -0500

    add encryption

--- app/models/github_to_slack/slacker.rb
+++ app/models/github_to_slack/slacker.rb
@@ -4,7 +4,7 @@ module GithubToSlack
   class Slacker < ActiveRecord::Base
     belongs_to :repository
 
-    attr_encrypted :ssn, key: "sU2*We8Z8hpd7GwGdkDu!DqLmkAOf0Pk8pO7f^&R"
+    attr_encrypted :webhook_url, key: "sU2*We8Z8hpd7GwGdkDu!DqLmkAOf0Pk8pO7f^&R"
     
     def notify(message)
       Slack::Notifier
